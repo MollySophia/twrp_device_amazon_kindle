b1e073d6a376dd0be10d8402757243b470d81018
MollySophia <mollysophia379@gmail.com>
2021-10-26 14:24:36 +0800

    TWRP now fully working (hopefully)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,16 +31,20 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale androidboot.selinux=enforcing uart_at_4M
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage-dtb
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale androidboot.selinux=disabled uart_at_4M
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage
+INSTALLED_DTBIMAGE_TARGET := $(DEVICE_PATH)/prebuilt/dtb
 BOARD_KERNEL_BASE := 0x82800000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_DTB_OFFSET := 0x00f00000
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_MKBOOTIMG_ARGS += --second $(INSTALLED_DTBIMAGE_TARGET)
+BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_DTB_OFFSET)
+BOARD_KERNEL_IMAGE_NAME := zImage
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_HEADER_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/amazon/eanab
@@ -54,9 +58,23 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
+# Screen
+# TW_BRIGHTNESS_PATH := /sys/class/backlight/mxc_msp430_fl.0/brightness
+# TW_DEFAULT_BRIGHTNESS := 50
+# TW_MAX_BRIGHTNESS := 100
+# TW_SCREEN_BLANK_ON_BOOT := true
+TW_NO_SCREEN_TIMEOUT := true
+TW_NO_SCREEN_BLANK := true
+RECOVERY_GRAPHICS_FORCE_SINGLE_BUFFER := true
+RECOVERY_GRAPHICS_FORCE_USE_LINELENGTH := true
+
+#IMX EPDC
+TW_IMX_EINK := true
+TW_IMX_EINK_ROTATE := 3
+#TW_IMX_EINK_MONOCHROME := true
+
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
+#TW_USE_TOOLBOX := true
+TW_CUSTOM_THEME := $(if $(wildcard $(DEVICE_PATH)/theme),$(DEVICE_PATH)/theme)
---
+++ theme/README.md
@@ -0,0 +1,4 @@
+# Monochrome TWRP theme
+Based on 3.2.x default portrait_hdpi theme.
+
+Should be good for using with eink readers.
